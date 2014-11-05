8ef4697e4bcc8c2f0ec7e6f184cec449a67a4ffd
Armen123 <Armen.90_90@mail.ru>
2014-11-05 21:23:57 +0300

    test na unity

---
+++ Project/Assets/MainMenu.js
@@ -0,0 +1,14 @@
+﻿var Play = false;
+var Settings = false;
+var Quit = false;
+
+function onMouseEnter()
+{
+  renderer.material.color = Color.red;
+  
+}
+function onMouseExit()
+{
+  renderer.material.color = Color.white;
+  
+}
